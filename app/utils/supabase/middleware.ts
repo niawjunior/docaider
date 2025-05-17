31a4b65de54e9ebaf67da555ff77e88e0ef5ad22
niawjunior <niawkung@gmail.com>
2025-05-17 18:59:05 +0700

    chore: improve ui

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -41,7 +41,9 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/share") &&
+    !request.nextUrl.pathname.startsWith("/api/share")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
