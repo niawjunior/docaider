382f29992e853442077df422cd9018623c037fd6
niawjunior <niawkung@gmail.com>
2025-05-20 10:39:13 +0700

    fix: create id

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { createChat } from "../utils/aisdk/chat";
+import GlobalLoader from "../components/GlobalLoader";
 
 export default function Chat() {
-  createChat().then((chatId) => {
-    redirect(`/chat/${chatId}`);
-  });
+  const router = useRouter();
+
+  useEffect(() => {
+    async function initChat() {
+      const chatId = await createChat();
+      router.push(`/chat/${chatId}`);
+    }
+    initChat();
+  }, [router]);
+
+  return <GlobalLoader />;
 }
