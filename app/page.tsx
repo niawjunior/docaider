4a69f11469c8d4087027590d0a4aaad9fefe92d4
niawjunior <niawkung@gmail.com>
2025-06-07 19:38:42 +0700

    feat: improve ui

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 
   return (
     <div className="w-full h-screen flex flex-col">
-      <div className="absolute top-0 w-full px-6 py-4 flex justify-between items-center">
+      <div className="absolute top-0 w-full px-6 py-4 flex justify-between items-center z-50 ">
         <Link href="/">
           <span className="text-white text-xl font-bold">✨ DocAider</span>
         </Link>
