49168b45d7cb6d173e842b581d467d19ba3427f4
niawjunior <niawkung@gmail.com>
2025-11-26 10:07:13 +0700

    chore: Remove the entire docaider-react-chat-example project and its associated files.

--- docaider-react-chat-example/dist/components/ChatButton.d.ts
+++
@@ -1,9 +0,0 @@
-import { ChatConfig } from "../types";
-interface ChatButtonProps {
-    isOpen: boolean;
-    config: ChatConfig;
-    onToggle: () => void;
-}
-export declare function ChatButton({ isOpen, config, onToggle }: ChatButtonProps): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=ChatButton.d.ts.map
--- docaider-react-chat-example/dist/components/MessageInput.d.ts
+++
@@ -1,9 +0,0 @@
-interface MessageInputProps {
-    onSendMessage: (message: string) => void;
-    placeholder?: string;
-    disabled?: boolean;
-    primaryColor?: string;
-}
-export declare function MessageInput({ onSendMessage, placeholder, disabled, primaryColor, }: MessageInputProps): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=MessageInput.d.ts.map
